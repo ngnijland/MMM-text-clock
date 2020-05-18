ec3ce92ad7a6d695ce353e7208011889c77722c4
Niek Nijland <ngnijland@gmail.com>
2020-05-18 22:24:44 +0200

    feat: add internationalization functionality

---
+++ node_helper.js
@@ -0,0 +1,23 @@
+const NodeHelper = require('node_helper');
+const locale = require('./locale');
+
+module.exports = NodeHelper.create({
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === 'SET_LANGUAGE') {
+      this.sendSocketNotification(
+        'SET_LANGUAGE',
+        JSON.stringify(
+          locale[payload],
+          (_, value) => {
+            if (typeof value == 'function') {
+              return '__FUNC__' + value.toString();
+            }
+
+            return value;
+          },
+          2
+        )
+      );
+    }
+  },
+});
