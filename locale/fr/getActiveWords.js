78211d2f0c97c9075552f6e803c96ee183df514c
Pierre-Philippe di Costanzo <pierrephi@gmail.com>
2021-01-30 01:19:14 +0100

    Migrated French to centralised display time calculation

--- locale/fr/getActiveWords.js
+++ locale/fr/getActiveWords.js
@@ -1,56 +1,35 @@
 module.exports = function (time) {
   const wordIndexes = [];
+  const display = this.displayTime(time);
 
   wordIndexes.push(this.wordMap.il);
   wordIndexes.push(this.wordMap.est);
 
-  const hours = time.getHours();
-  const minutes = time.getMinutes();
-
-  if ((minutes >= 3 && minutes <= 7) || (minutes >= 53 && minutes <= 57)) {
+  if (Math.abs(display.minutes_to_display) === 5) {
     wordIndexes.push(this.wordMap.cinq);
-  } else if (
-    (minutes >= 8 && minutes <= 12) ||
-    (minutes >= 48 && minutes <= 52)
-  ) {
+  } else if (Math.abs(display.minutes_to_display) === 10) {
     wordIndexes.push(this.wordMap.dix);
-  } else if (minutes >= 13 && minutes <= 17) {
+  } else if (display.minutes_to_display === 15) {
     wordIndexes.push(this.wordMap.etquart);
-  } else if (minutes >= 43 && minutes <= 47) {
+  } else if (display.minutes_to_display === -15) {
     wordIndexes.push(this.wordMap.lequart);
-  } else if (
-    (minutes >= 18) & (minutes <= 22) ||
-    (minutes >= 38 && minutes <= 42)
-  ) {
+  } else if (Math.abs(display.minutes_to_display) === 20) {
     wordIndexes.push(this.wordMap.vingt);
-  } else if (
-    (minutes >= 23 && minutes <= 27) ||
-    (minutes >= 33 && minutes <= 37)
-  ) {
+  } else if (Math.abs(display.minutes_to_display) === 25) {
     wordIndexes.push(this.wordMap.vingtcinq);
-  } else if (minutes >= 28 && minutes <= 32) {
+  } else if (display.minutes_to_display === 30) {
     wordIndexes.push(this.wordMap.etdemie);
   }
 
-  if (minutes >= 33 && minutes <= 57) {
+  if (display.minutes_to_display < 0) {
     wordIndexes.push(this.wordMap.moins);
   }
 
-  const displayHour = minutes >= 33 ? hours + 1 : hours;
+  wordIndexes.push(this.wordMap[display.hours_to_display]);
 
-  if (displayHour === 12) {
-    wordIndexes.push(this.wordMap.midi);
-  } else if (displayHour % 24 === 0) {
-    wordIndexes.push(this.wordMap.minuit);
-  } else {
-    wordIndexes.push(
-      this.wordMap[displayHour > 12 ? displayHour - 12 : displayHour]
-    );
-  }
-
-  if (displayHour % 12 === 0) {
+  if (display.hours_to_display % 12 === 0) {
     // noop midnight/noon
-  } else if (displayHour % 12 === 1) {
+  } else if (display.hours_to_display === 1) {
     wordIndexes.push(this.wordMap.heure);
   } else {
     wordIndexes.push(this.wordMap.heures);
